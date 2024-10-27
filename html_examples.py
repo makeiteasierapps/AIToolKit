157049da7bbdc76e7e2730833cd09b167fabed72
Shaun Offenbacher <makeiteasierapps@gmail.com>
2024-10-26 19:07:59 -0600

    made it better

--- html_examples.py
+++ html_examples.py
@@ -13,7 +13,9 @@ examples = {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
             
             <!-- Font Awesome CSS -->
-            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+            <!-- Update to Font Awesome version 6 -->
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
             
         <body>
             {body}
--- site_builder.py
+++ site_builder.py
@@ -7,19 +7,31 @@ from dotenv import load_dotenv
 import requests
 from html_examples import examples
 import os
-from dspy import  LM, configure, InputField, OutputField, Signature, TypedChainOfThought, ChainOfThought
+from dspy import  LM, configure, InputField, OutputField, Signature, TypedChainOfThought, ChainOfThought, context
 
 load_dotenv()
 os.environ.get("OPENAI_API_KEY")
-lm = LM('openai/gpt-4o-mini', max_tokens=5000)
+os.environ.get("ANTHROPIC_API_KEY")
+model_dict = {
+    'sonnet': 'anthropic/claude-3-5-sonnet-latest',
+    'haiku': 'anthropic/claude-3-haiku-20240307',
+    'opus': 'anthropic/claude-3-opus-latest',
+    '4o-mini': 'openai/gpt-4o-mini',
+    '4o': 'openai/gpt-4o',
+}
+lm = LM(model_dict['haiku'], max_tokens=4096)
 configure(lm=lm)
+strong_lm = LM(model_dict['sonnet'], max_tokens=8000)
+
+class Nav(BaseModel):
+    need_nav: bool
 
 class ImageQuery(BaseModel):
-    background_image_query: str
-    related_image_query: str
+    design_related_image: str
+    theme_related_image: str
 
 class Images(BaseModel):
-    instructions: str
+    section_name: str
     url: str
 
 class ImageInstructions(BaseModel):
@@ -36,11 +48,11 @@ class SectionInstructions(BaseModel):
 
 class ClassifyImages(Signature):
     '''You are given a dictionary that contains image descriptions as keys and urls as values.
-    You are also given a set of web site design instructions.
-    Your job is to return a string that contains the image urls and instructions on where or how to use them in the web site.'''
+    You are also given a section names of a website, ie. hero, about, services, etc.
+    '''
     image_descriptions = InputField()
-    design_instructions = InputField()
-    image_instructions: ImageInstructions = OutputField(desc="A list of image objects that contain the instructions and urls")
+    section_names = InputField()
+    image_instructions: ImageInstructions = OutputField()
 
 class CreateInstructions(Signature):
     '''
@@ -82,8 +94,9 @@ class CreateInstructions(Signature):
         Responsive Embeds: For videos or iframes, use Bootstrap’s ratio classes (e.g., ratio-16x9) to maintain a responsive aspect ratio.
     '''
     description = InputField(desc='The user\'s description of the website')
-    image_query: ImageQuery = OutputField(desc='1-3 word key phrase used to search images.')
+    image_query: ImageQuery = OutputField(desc='usually 1-3 words')
     website_title = OutputField(desc='The title of the website')
+    need_nav: Nav = OutputField()
     instructions_list: SectionInstructions = OutputField(desc='A list of verbose section instructions that contain the section name, instructions, and class name')
     color_scheme = OutputField(desc='A guide to the color scheme for the website')
 
@@ -94,19 +107,29 @@ class CSSRules(Signature):
     color_scheme = InputField()
     css_rules = OutputField(desc='The CSS color scheme and rules for the website contained within <style> tags')
 
-class HTMLBody(Signature):
+class NavInstuctions(Signature):
     '''
-        Please create the <section> of a website using Bootstrap 5 elements and classes, based on the user's instructions. 
+    You are given JSON of section instructions. Please return the updated list with a navigation section added to the beginning.
+    '''
+    section_instructions = InputField(desc='The JSON of section instructions')
+    updated_instructions: SectionInstructions = OutputField()
+
+class HTMLElement(Signature):
+    '''
+        Please create the needed HTML element wrapping your response in the necessary tag, ie <nav> or <section>. 
+        Use Bootstrap 5 elements and classes, based on the user's instructions. 
         Generate meaningful and relevant text based on the user's description to use in the page.
 
+        - Use the image url provided in the images field if it exists.
         - Ensure you are using modern Bootstrap elements and classes for the layout.
         - The output should contain meaningful content that corresponds to the user's description.
     '''
     description = InputField(desc='The user\'s description of the website')
-    class_name = InputField(desc='The class name for the section')
+    class_name = InputField()
+    images = InputField(desc='List of images or None')
     html = OutputField(desc='The HTML code for the given section')
 
-def query_unsplash(query, per_page=10, page=1):
+def query_unsplash(query, per_page=30, page=1):
     url = "https://api.unsplash.com/search/photos"
     headers = {
         "Authorization": f"Client-ID {os.environ.get('UNSPLASH_ACCESS_KEY')}"
@@ -140,47 +163,63 @@ def clean_html(html):
     return clean_text
 
 def promptify(prompt):
+    print(prompt)
+    # Create HTML scaffold
+    html_scaffold = examples['scaffold']
+    current_html = html_scaffold.format(website_title='', css_style_element='', body='')
+    yield current_html
+    
     # Create instructions
-    create_instructions = TypedChainOfThought(CreateInstructions)
-    instructions_response = create_instructions(description=prompt)
+    with context(lm=strong_lm):    
+        create_instructions = TypedChainOfThought(CreateInstructions)
+        instructions_response = create_instructions(description=prompt)
+
     website_instructions = instructions_response.instructions_list
     style_instructions = instructions_response.color_scheme
+    website_title = instructions_response.website_title
+    image_query = instructions_response.image_query
+    need_nav = instructions_response.need_nav.need_nav
     website_dict = website_instructions.model_dump()
+
     section_instructions = website_dict['section_instructions']
-    pprint.pprint(section_instructions)
     section_instructions_str = json.dumps(section_instructions)
     
     css_rules = ChainOfThought(CSSRules)
     css_rules_response = css_rules(section_instructions=section_instructions_str, color_scheme=style_instructions)
     css_style_element = css_rules_response.css_rules
-
-    website_title = instructions_response.website_title
-    image_query = instructions_response.image_query
-    image_query_dict = image_query.model_dump()
-    background_image_query = image_query_dict['background_image_query']
-    related_image_query = image_query_dict['related_image_query']
     
-    # # Get images
-    # image_results = query_unsplash(image_query)
-    # image_dict = extract_image_data(image_results)
-    # image_dict_str = json.dumps(image_dict)
-    # classify_images = ChainOfThought(ClassifyImages)
-    # classify_imgs_response = classify_images(image_descriptions=image_dict_str, design_instructions=website_instructions)
-    # image_instructions = classify_imgs_response.image_instructions
-    # image_instructions_json = json.dumps(image_instructions.model_dump())
-    
-    # Create HTML scaffold
-    html_scaffold = examples['scaffold']
     current_html = html_scaffold.format(website_title=website_title, css_style_element=css_style_element, body='')
     
+    image_query_dict = image_query.model_dump()
+    theme_related_image_query = image_query_dict['theme_related_image']
+    
+    # Get images
+    theme_related_image_results = query_unsplash(theme_related_image_query)
+    theme_related_image_dict = extract_image_data(theme_related_image_results)
+    
+    theme_related_image_dict_str = json.dumps(theme_related_image_dict)
+    classify_images = TypedChainOfThought(ClassifyImages)
+    section_names_str = json.dumps([section['section_name'] for section in section_instructions])
+    classify_imgs_response = classify_images(image_descriptions=theme_related_image_dict_str, section_names=section_names_str)
+    image_instructions = classify_imgs_response.image_instructions
+    
+    # Get image instructions as a dictionary for easier lookup
+    image_lookup = {img.section_name: img.url for img in image_instructions.image_instructions}
+    
+    if need_nav:
+        nav_instructions = TypedChainOfThought(NavInstuctions)
+        nav_instructions_response = nav_instructions(section_instructions=section_instructions_str)
+        section_instructions = nav_instructions_response.updated_instructions.model_dump()['section_instructions']
     # Build and insert each section progressively
-    for section in section_instructions:
-        section_html = TypedChainOfThought(HTMLBody)
-        section_html_response = section_html(css_rules=style_instructions, description=section['instructions'], class_name=section['class_name'])
-        clean_section = clean_html(section_html_response.html)
-        
-        # Find the closing body tag and insert the new section before it
-        body_end_pos = current_html.find('</body>')
-        current_html = current_html[:body_end_pos] + clean_section + current_html[body_end_pos:]
+    
+    with context(lm=strong_lm):
+        for section in section_instructions:
+            section_html = TypedChainOfThought(HTMLElement)
+            section_html_response = section_html(css_rules=style_instructions, description=section['instructions'], class_name=section['class_name'], images=image_lookup.get(section['section_name'], 'None'))
+            clean_section = clean_html(section_html_response.html)
+            
+            # Find the closing body tag and insert the new section before it
+            body_end_pos = current_html.find('</body>')
+            current_html = current_html[:body_end_pos] + clean_section + current_html[body_end_pos:]
 
-        yield current_html
+            yield current_html
